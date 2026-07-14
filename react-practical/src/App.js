04974908895f3208b8ce38627663b38c596fa29f
riya radadiya <riyaradadiya9@gmail.com>
2026-07-14 15:15:15 +0530

    Added React practical

---
+++ react-practical/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to React</h1>
+
+      <p>
+        My first React application for internship practical.
+      </p>
+
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
