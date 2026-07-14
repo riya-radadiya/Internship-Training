f64f9b28888b900c8e41746a0f4b617c559c5f1c
riya radadiya <riyaradadiya9@gmail.com>
2026-07-14 16:55:23 +0530

    Add React and Next.js practicals

---
+++ next-practical/app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        textAlign: "center",
+        marginTop: "100px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>Welcome to Next.js</h1>
+      <p>This is my first Next.js application.</p>
+    </main>
+  );
+}
