41533dcb7ac8596dc6a1a695b825d9f3a9e6d6a0
riya radadiya <riyaradadiya9@gmail.com>
2026-07-14 16:24:48 +0530

    Added Conditional Rendering practical

--- react-practical/src/App.js
+++ react-practical/src/App.js
@@ -1,9 +1,31 @@
 
 
-import Todo from "./Todo";
+import { useState } from "react";
 
 function App() {
-  return <Todo />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      {isLoggedIn ? (
+        <>
+          <h1>Welcome, Riya!</h1>
+          <p>You are successfully logged in.</p>
+          <button onClick={() => setIsLoggedIn(false)}>
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <h1>Login Page</h1>
+          <p>Please login to continue.</p>
+          <button onClick={() => setIsLoggedIn(true)}>
+            Login
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
